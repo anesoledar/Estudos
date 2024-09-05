40e596e16ea6f0002d665eef8c3f80a104d58c10
anesoledar <ane.soledar@gmail.com>
2024-09-05 11:50:04 -0300

    Código 02-node-fundamentos atualizado até o fim do módulo 'Estrutura da Aplicação'

--- Rocketseat/Node-js/02-node-fundamentos/src/server.js
+++ Rocketseat/Node-js/02-node-fundamentos/src/server.js
@@ -1,7 +1,30 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((request, response) => {
-    return response.end("Seja bem vindo!!!")
+    const {method, url} = request
+
+    if (method == 'GET' && url == '/users') {
+        return response
+        .writeHead(200)
+        .setHeader('Content-type', 'application/json')
+        .end(JSON.stringify(users))
+    }
+
+    if (method == 'POST' && url == '/users') {
+        users.push({
+            id: 1,
+            name: 'Jon Doe',
+            email: 'join.doe@exemple.com'})
+
+        return response.writeHead(201).end("Criação de Usuário")
+
+    }
+
+    return response.writeHead(404).end()
 })
 
 server.listen(3333)
+
+
