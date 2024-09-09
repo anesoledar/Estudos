f40f1f261663804501b5bfc3dbdab116effc0665
anesoledar <ane.soledar@gmail.com>
2024-09-09 13:10:22 -0300

    Atualiza 01-node-fundamentos até o final do modulo '03-streams do node.js', adiciona pasta middleware

--- Rocketseat/Node-js/01-node-fundamentos/src/server.js
+++ Rocketseat/Node-js/01-node-fundamentos/src/server.js
@@ -1,22 +1,28 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 const users = []
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async(request, response) => {
     const {method, url} = request
+
+    await json(request, response)
+
+    console.log(request.body)
 
     if (method == 'GET' && url == '/users') {
         return response
-        .writeHead(200)
         .setHeader('Content-type', 'application/json')
         .end(JSON.stringify(users))
     }
 
     if (method == 'POST' && url == '/users') {
+        const {name, email} = request.body
+
         users.push({
             id: 1,
-            name: 'Jon Doe',
-            email: 'join.doe@exemple.com'})
+            name,
+            email})
 
         return response.writeHead(201).end("Criação de Usuário")
 
